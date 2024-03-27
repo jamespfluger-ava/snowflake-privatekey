49e967d43a92286773306addd046819a0319905e
James Pfluger <james.pfluger@avalara.com>
2024-03-27 13:27:22 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Snowflake.Data.Client;
+
+string keyText = File.ReadAllText(@"C:\.keys\snowflake_key.p8").ReplaceLineEndings("\n");
+string keyPwd = Environment.GetEnvironmentVariable("SNOWFLAKE_KEY_PASSPHRASE");
+
+SnowflakeDbConnectionStringBuilder connStringBuilder = new SnowflakeDbConnectionStringBuilder()
+{
+    ["ACCOUNT"] = "ACCOUNT_NAME",
+    ["DB"] = "DB_NAME",
+    ["SCHEMA"] = "SCEHMA_NAME",
+    ["USER"] = "USER_NAME",
+    ["ROLE"] = "ROLE_NAME",
+    ["WAREHOUSE"] = "WH_NAME",
+    ["AUTHENTICATOR"] = "SNOWFLAKE_JWT",
+    ["PRIVATE_KEY"] = keyText,
+    ["PRIVATE_KEY_PWD"] = keyPwd,
+    ["CLIENT_CONFIG_FILE"] = @"C:\snowflake-example\sf_client_config.json"
+};
+
+SnowflakeDbConnection conn = new SnowflakeDbConnection();
+conn.ConnectionString = connStringBuilder.ConnectionString;
+conn.Open();
+
+Console.WriteLine("Connection successful!");
---
+++ README.md
@@ -0,0 +1,11 @@
+## Snowflake Private Key Example
+
+This repo is to show how to connect to Snowflake using a private key
+
+### Manual build steps
+
+_This section assumes you have replaced the 
+
+1. Navigate your terminal into the root of the repo
+2. Run `dotnet build SnowflakePrivateKey.csproj`
+3. Run `dotnet snowflake-example\bin\Debug\net6.0\SnowflakePrivateKey.dll`
